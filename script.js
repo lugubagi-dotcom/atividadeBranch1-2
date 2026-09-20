dee69594fc02add6a171d37ae3083f7b36c205e7
lugubagi-dotcom <lugubagi@gmail.com>
2026-09-20 13:40:00 +0000

    fix: impede adicionar tarefa com campo de texto vazio

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const taskList = document.getElementById("task-list");
 
 // Bug corrigido: o botão "Adicionar" não tinha nenhuma ação associada.
 addBtn.addEventListener("click", () => {
+  if (input.value.trim() === "") return; // bug corrigido: adicionava tarefa em branco
+
   const li = document.createElement("li");
   li.textContent = input.value;
   taskList.appendChild(li);

3dc2a73aef5cd47990fb1d2439831265517c3ac3
Jean-2009 <jean.santana2@estudante.ifms.edu.br>
2026-09-21 09:00:00 +0000

    feat: adiciona botões de filtro e marcação de tarefa concluída

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ addBtn.addEventListener("click", () => {
 
   const li = document.createElement("li");
   li.textContent = input.value;
+  li.addEventListener("click", () => li.classList.toggle("completed"));
   taskList.appendChild(li);
   input.value = ""; // bug corrigido: campo não era limpo após adicionar
 });
